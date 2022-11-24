f1906b0840563415446297070c0eb17583c993d2
Md-Hasan-raza <111636838+Md-Hasan-raza@users.noreply.github.com>
2022-11-24 11:53:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,17 +9,22 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
-
+   
+    <!----------------Bank--------------->
+    
     <title>Basic Banking System</title>
   </head>
 
   <body>
   <?php
+    
+    
   include 'navbar.php';
   ?>
   
       <div class="container-fluid">
-      <!-- Introduction section -->
+      <!------------------------------------------- Introduction section --------------------------------->
+        
             <div class="row intro py-1" style="background-color : #82E0AA;">
               <div class="col-sm-12 col-md">
                 <div class="heading text-center my-5">
@@ -32,7 +37,8 @@
               </div>
             </div>
 
-      <!-- Activity section -->
+      <!----------------------------------- Activity section ----------------------------->
+        
             <div class="row activity text-center">
                   <div class="col-md act">
                     <img src="img/user.jpg" class="img-fluid">
@@ -52,7 +58,7 @@
             </div>
       </div>
       <footer class="text-center mt-5 py-2">
-      <p>&copy 2021. Made by <b>Kumar Anket</b></p>
+      <p>&copy 2022. Made by <b>Md Hasan Raza</b></p>
       </footer>
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
